b0929074f12577d8004f50713c3c4b29ad3b0f7f
Jack Duignan <JackpDuignan@gmail.com>
2024-06-27 20:44:18 +1200

    add: main file

---
+++ src/main.py
@@ -0,0 +1,24 @@
+##
+# @file main.py
+# @author Jack Duignan (JackpDuignan@gmail.com)
+# @date 27-06-2024
+# @brief the main file for the better-terminal app containing the main loop 
+# and helper Functions
+
+
+import serial
+
+
+
+def main() -> None:
+    """
+    The main function for the project 
+    """
+    
+
+
+if __name__ == "__main__":
+    main()
+
+
+
